d20e7d212fad81986aa9ea100122852aebd9a7b8
silent-box <silent-box@ya.ru>
2016-03-27 15:53:15 +0400

    Update AccountServiceApplication.java

--- account-service/src/main/java/com/test/account/AccountServiceApplication.java
+++ account-service/src/main/java/com/test/account/AccountServiceApplication.java
@@ -1,10 +1,14 @@
 package com.test.account;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
 
 @SpringBootApplication
 @EnableResourceServer
